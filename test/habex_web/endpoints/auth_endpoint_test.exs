2c3bca596fbbffe6cb2389d142c00c9d7813201b
Unknown <richardpanda1993@gmail.com>
2017-08-19 23:47:55 -0700

    Create tests for POST /auth/signin

--- test/habex_web/endpoints/auth_endpoint_test.exs
+++ test/habex_web/endpoints/auth_endpoint_test.exs
@@ -1,7 +1,49 @@
 defmodule HabexWeb.AuthEndpointTest do
   use HabexWeb.ConnCase
 
+  alias Comeonin.Bcrypt
   alias Habex.{Repo, User}
+
+  describe "POST /auth/signin" do
+    test "Send 200 for valid credentials" do
+      password = "password"
+      password_hash = Bcrypt.hashpwsalt(password)
+      User.changeset(%User{}, %{email: "test@test.com", password_hash: password_hash})
+      |> Repo.insert()
+
+      req_body = [email: "test@test.com", password: password]
+      conn = post build_conn(), "/auth/signin", req_body
+      resp_body = Poison.decode! conn.resp_body
+
+      assert conn.status == 200
+      assert resp_body["email"] == "test@test.com"
+      assert resp_body["id"]
+      assert resp_body["token"]
+    end
+
+    test "Send 400 for unregistered email" do
+      req_body = [email: "test@test.com", password: "password"]
+      conn = post build_conn(), "/auth/signin", req_body
+      resp_body = Poison.decode! conn.resp_body
+
+      assert conn.status == 400
+      assert resp_body["message"] == "Invalid email and/or password."
+    end
+
+    test "Send 400 for invalid password" do
+      password = "password"
+      password_hash = Bcrypt.hashpwsalt(password)
+      User.changeset(%User{}, %{email: "test@test.com", password_hash: password_hash})
+      |> Repo.insert()
+
+      req_body = [email: "test@test.com", password: "123456"]
+      conn = post build_conn(), "/auth/signin", req_body
+      resp_body = Poison.decode! conn.resp_body
+
+      assert conn.status == 400
+      assert resp_body["message"] == "Invalid email and/or password."
+    end
+  end
 
   describe "POST /auth/signup" do
     test "Creates and responds with a newly created user if attributes are valid" do
